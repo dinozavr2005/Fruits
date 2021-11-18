6fefcc224f22508b21811c79aab1724c28927dbe
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-11-18 12:43:37 +0300

    first commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,21 @@
+//
+//  Created by Владимир on 08.10.2021.
+//
+
+
+import SwiftUI 
+
+@main
+struct FructusApp: App {
+  @AppStorage("isOnboarding") var isOnboarding: Bool = true
+  
+  var body: some Scene {
+    WindowGroup {
+      if isOnboarding {
+        OnboardingView()
+      } else {
+        ContentView()
+      }
+    }
+  }
+}
